67f0b57e500ce8c76ea86d6b5d9358d0564d3a1e
Martin <155843922+cap89@users.noreply.github.com>
2024-02-22 15:56:47 +0100

    update deleting lists possible

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -20,9 +20,19 @@ class ListsController < ApplicationController
     end
   end
 
+  def destroy
+    set_list
+    @list.destroy
+    redirect_to lists_path, notice: 'List was successfully destroyed.'
+  end
+
   private
 
   def list_params
     params.require(:list).permit(:name)
   end
+
+def set_list
+  @list = List.find(params[:id])
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'lists/new', to: 'lists#new', as: 'new_list'
   post 'lists', to: 'lists#create'
   get 'lists/:id', to: 'lists#show', as: 'list'
+  delete 'lists/:id', to: 'lists#destroy'
 
 
   get 'bookmarks/new', to: 'bookmarks#new', as: 'new_bookmark'
